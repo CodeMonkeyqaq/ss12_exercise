a3f1ec88584d2e237b99314a9687648e3baac336
CodeMonkeyqaq <1242532947@qq.com>
2018-05-21 19:09:53 +0800

    Initial Commit

---
+++ ss12_exercise/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ss12_exercise
+//
+//  Created by 王星洲 on 2018/5/21.
+//  Copyright © 2018年 cn.edu.tongji.1652977. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
